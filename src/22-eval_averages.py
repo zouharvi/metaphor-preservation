9efb2a00071eb2f4135e77572f4de049c94666ca
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-06-20 22:46:37 +0200

    new avgs

--- src/22-eval_averages.py
+++ src/22-eval_averages.py
@@ -3,16 +3,20 @@
 import json
 import numpy as np
 
-data = [json.loads(x) for x in open("data/output_eval/paraphrase_pegasus.jsonl", "r")]
+data = [json.loads(x) for x in open("data/eval_present/dataset.jsonl", "r")]
 
 trofi_lit_vals = [float(x["text_lit"]) for x in data if x["dataset"]=="trofi" and x["text_lit"]]
 trofi_met_vals = [float(x["text_met"]) for x in data if x["dataset"]=="trofi" and x["text_met"]]
 fmo_lit_vals = [float(x["text_lit"]) for x in data if x["dataset"]=="fmo"]
 fmo_met_vals = [float(x["text_met"]) for x in data if x["dataset"]=="fmo"]
-fmo_met_gt_lit = [float(x["text_met"]) >= float(x["text_lit"]) for x in data if x["dataset"]=="fmo"]
+fmo_met_geq_lit = [float(x["text_met"]) >= float(x["text_lit"]) for x in data if x["dataset"]=="fmo"]
+fmo_met_gt_lit = [float(x["text_met"]) > float(x["text_lit"]) for x in data if x["dataset"]=="fmo"]
+fmo_met_eq_lit = [float(x["text_met"]) == float(x["text_lit"]) for x in data if x["dataset"]=="fmo"]
 
 print(f"Trofi LIT avg: {np.average(trofi_lit_vals):.2f}")
 print(f"Trofi MET avg: {np.average(trofi_met_vals):.2f}")
 print(f"FMO LIT avg: {np.average(fmo_lit_vals):.2f}")
 print(f"FMO MET avg: {np.average(fmo_met_vals):.2f}")
-print(f"FMO MET>=LIT: {np.average(fmo_met_gt_lit):.2%}")
+print(f"FMO MET>=LIT: {np.average(fmo_met_geq_lit):.2%}")
+print(f"FMO MET>LIT: {np.average(fmo_met_gt_lit):.2%}")
+print(f"FMO MET=LIT: {np.average(fmo_met_eq_lit):.2%}")
